3f99e9d520530e7485796ed5816296c86203fe2b
edwin aguiar <vallecondo@gmail.com>
2026-02-23 01:11:07 +0000

    feat: cambio oficial a EDUControlPro com.educontrolpro y fix pantalla blanca

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,6 +1,12 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
+
 const config: CapacitorConfig = {
   appId: 'com.educontrolpro',
   appName: 'EDUControlPro',
   webDir: 'out',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
 export default config;
