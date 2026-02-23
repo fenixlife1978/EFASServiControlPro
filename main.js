925723ccc4411dbcb6977a5ea03392a795dea036
fenixlife1978 <generaextra@gmail.com>
2026-02-22 22:36:11 -0400

    Initialize Electron app with main window setup

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-"Cuando el ususario haga doble click, abre una ventana de 1200x800 y muestra mi web"
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const fs = require('fs');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  // Aquí le decimos que cargue tu web de Next.js
+  // Cuando lo instales, buscará la carpeta "out" que genera Next.js
+  const isDev = process.env.NODE_ENV === 'development';
+  
+  if (isDev) {
+    win.loadURL('http://localhost:3000');
+  } else {
+    win.loadFile(path.join(__dirname, 'out/index.html'));
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
