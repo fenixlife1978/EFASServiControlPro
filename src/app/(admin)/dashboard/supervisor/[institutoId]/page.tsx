6a0849587f9554ad8b5769e729d3884c3eaf47c7
edwin aguiar <vallecondo@gmail.com>
2026-05-16 01:39:59 +0000

    Corrige página de supervisor sin ruta dinamica

--- src/app/(admin)/dashboard/supervisor/[institutoId]/page.tsx
+++
@@ -1,9 +0,0 @@
-import SedeMonitorClient from './SedeMonitorClient';
-
-export async function generateStaticParams() {
-  return [];
-}
-
-export default function Page({ params }: { params: { institutoId: string } }) {
-  return <SedeMonitorClient institutoId={params.institutoId} />;
-}
